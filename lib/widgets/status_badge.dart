fc7c00a599b95d7ae35d43c8665ae133ef4867dc
sandeepyadav919 <sandeepyadav919@gmail.com>
2026-08-06 14:30:27 +0530

    Add reusable StatusBadge widget

---
+++ lib/widgets/status_badge.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class StatusBadge extends StatelessWidget {
+  final String status;
+
+  const StatusBadge({
+    super.key,
+    required this.status,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Color backgroundColor;
+    Color textColor = Colors.white;
+
+    switch (status.toLowerCase()) {
+      case 'live':
+        backgroundColor = Colors.red;
+        break;
+
+      case 'upcoming':
+        backgroundColor = Colors.orange;
+        break;
+
+      case 'finished':
+        backgroundColor = Colors.green;
+        break;
+
+      default:
+        backgroundColor = Colors.grey;
+    }
+
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 12,
+        vertical: 6,
+      ),
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Text(
+        status.toUpperCase(),
+        style: TextStyle(
+          color: textColor,
+          fontWeight: FontWeight.bold,
+          fontSize: 12,
+        ),
+      ),
+    );
+  }
+}
